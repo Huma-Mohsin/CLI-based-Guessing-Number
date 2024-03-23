a3a1b2d22a98285a82c7e7cc9e9b01fa4f986e94
Huma-Mohsin <160951214+Huma-Mohsin@users.noreply.github.com>
2024-03-24 04:34:09 +0500

    push CLI Game

---
+++ index.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+//creating a CLI based GUESSING NUMBER GAME
+import inquirer from "inquirer";
+import chalk from "chalk"
+import { log } from "console";
+let randomNumber=Math.floor(Math.random()*50+1)
+//console.log(randomNumber);//used for observation of flow(TESTING PURPOSE FOR CONDITIONS MATCH OR NOT)
+
+const answers=await inquirer.prompt([{
+    name:"GuessingNumber",
+    type:"number",
+    message:chalk.blueBright.bgYellow("Think of any Number and then Enter!")
+}])
+
+if(answers.GuessingNumber===randomNumber){
+    console.log(chalk.greenBright("Congratulations,You Won The Match!"));
+    
+}
+else{
+    console.log(chalk.redBright("Ooops!!! You Loss The Game,Try Again "));
+    
+}
+//Author-HUMA MOHSIN
